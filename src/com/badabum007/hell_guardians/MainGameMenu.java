915b0fae00eb2c4d4a1706b88d47afb2d749965c
Anton Stakhouskiy <badabum007@gmail.com>
2016-03-07 19:14:05 +0300

    icon and custom were added. added fixed stage size

--- src/com/badabum007/hell_guardians/MainGameMenu.java
+++ src/com/badabum007/hell_guardians/MainGameMenu.java
@@ -1,5 +1,8 @@
 package com.badabum007.hell_guardians;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -23,97 +26,110 @@ import javafx.stage.Stage;
 
 public class MainGameMenu extends Application {
 
-    private GameMenu gameMenu;
-//    private static Font font;
-    
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+	private GameMenu gameMenu;
 
-        Pane root = new Pane();
-        root.setPrefSize(800, 600);
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 
-        InputStream is = Files.newInputStream(Paths.get("res/images/main_menu.png"));
-        Image img = new Image(is);
-        is.close();
+		Pane root = new Pane();
+		root.setPrefSize(800, 600);
 
-        ImageView imgView = new ImageView(img);
-        imgView.setFitWidth(800);
-        imgView.setFitHeight(600);
+		InputStream is = Files.newInputStream(Paths.get("res/images/main_menu.png"));
+		Image img = new Image(is);
+		is.close();
 
-        gameMenu = new GameMenu();
-        gameMenu.setVisible(true);
+		ImageView imgView = new ImageView(img);
+		imgView.setFitWidth(800);
+		imgView.setFitHeight(600);
+		
+		primaryStage.setMinWidth(800);        
+		primaryStage.setMinHeight(600);
 
-        root.getChildren().addAll(imgView, gameMenu);
+		primaryStage.setMaxWidth(800);        
+		primaryStage.setMaxHeight(600);
+		
+		gameMenu = new GameMenu();
+		gameMenu.setVisible(true);
 
-        Scene scene = new Scene(root);
+		root.getChildren().addAll(imgView, gameMenu);
 
-        primaryStage.setTitle("Hell guardians");
-        
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+		Scene scene = new Scene(root);
 
-    private class GameMenu extends Parent {
-        public GameMenu() {
-            VBox menu0 = new VBox(10);
-            
-            menu0.setTranslateX(550);
-            menu0.setTranslateY(450);
+		primaryStage.setTitle("Hell guardians");
 
-            MenuButton btnResume = new MenuButton("Resume");   
-            MenuButton btnNewGame = new MenuButton("New game");       
+		primaryStage.setScene(scene);
+		primaryStage.getIcons().add(new Image("file:res/images/icon.png"));
+		primaryStage.show();
+	}
 
-            MenuButton btnExit = new MenuButton("Exit");
-            btnExit.setOnMouseClicked(event -> {
-                System.exit(0);
-            });
+	private class GameMenu extends Parent {
+		public GameMenu() throws FileNotFoundException {
+			VBox menu0 = new VBox(10);
 
-            menu0.getChildren().addAll(btnResume, btnNewGame, btnExit);
-            getChildren().add(menu0);
-        }
-    }
+			menu0.setTranslateX(550);
+			menu0.setTranslateY(450);
 
-    private static class MenuButton extends StackPane {
-        private Text text;
+			MenuButton btnResume = new MenuButton("Resume"); 
+			MenuButton btnNewGame = new MenuButton("New game");       
 
-        public MenuButton(String name) {
-            text = new Text(name);
-            text.getFont();
-			text.setFont(Font.font(20));
-            text.setFill(Color.WHITE);
+			MenuButton btnExit = new MenuButton("Exit");
+			btnExit.setOnMouseClicked(event -> {
+				System.exit(0);
+			});
 
-            Rectangle bg = new Rectangle(250, 30);
-            bg.setOpacity(0.6);
-            bg.setFill(Color.BLACK);
-            bg.setEffect(new GaussianBlur(3.5));
+			menu0.getChildren().addAll(btnResume, btnNewGame, btnExit);
+			getChildren().add(menu0);
+		}
+	}
 
-            setAlignment(Pos.CENTER_LEFT);
-            setRotate(-0.5);
-            getChildren().addAll(bg, text);
+	private static class MenuButton extends StackPane {
+		private Text text;
 
-            setOnMouseEntered(event -> {
-                bg.setTranslateX(10);
-                text.setTranslateX(10);
-                bg.setFill(Color.WHITE);
-                text.setFill(Color.BLACK);
-            });
+		public MenuButton(String name) {
+			text = new Text(name);
+			text.getFont();
+			Font font;
+			try {
+				font = Font.loadFont(new FileInputStream(new File("res/fonts/Kankin.otf")), 20);
+				text.setFont(font); 
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
 
-            setOnMouseExited(event -> {
-                bg.setTranslateX(0);
-                text.setTranslateX(0);
-                bg.setFill(Color.BLACK);
-                text.setFill(Color.WHITE);
-            });
+			text.setFill(Color.WHITE);
 
-            DropShadow drop = new DropShadow(50, Color.WHITE);
-            drop.setInput(new Glow());
+			Rectangle bg = new Rectangle(250, 30);
+			bg.setOpacity(0.6);
+			bg.setFill(Color.BLACK);
+			bg.setEffect(new GaussianBlur(3.5));
 
-            setOnMousePressed(event -> setEffect(drop));
-            setOnMouseReleased(event -> setEffect(null));
-        }
-    }
+			setAlignment(Pos.CENTER_LEFT);
+			setRotate(-0.5);
+			getChildren().addAll(bg, text);
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+			setOnMouseEntered(event -> {
+				bg.setTranslateX(10);
+				text.setTranslateX(10);
+				bg.setFill(Color.WHITE);
+				text.setFill(Color.BLACK);
+			});
+
+			setOnMouseExited(event -> {
+				bg.setTranslateX(0);
+				text.setTranslateX(0);
+				bg.setFill(Color.BLACK);
+				text.setFill(Color.WHITE);
+			});
+
+			DropShadow drop = new DropShadow(50, Color.WHITE);
+			drop.setInput(new Glow());
+
+			setOnMousePressed(event -> setEffect(drop));
+			setOnMouseReleased(event -> setEffect(null));
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
